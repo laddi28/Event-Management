4abbf73fa3bfc834cdb7ad31a6e3a1fc5c8f1011
laddi28 <priyanshutyagi2801@gmail.com>
2024-09-11 23:01:49 +0530

    Create Vendor.js

---
+++ models/Vendor.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const vendorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Vendor', vendorSchema);
